a247d74b330fe9184541e51885aafa68dac13918
Cossy <cosmasmutuku001@gmail.com>
2024-05-31 14:43:33 +0300

    test7

--- nexus/settings.py
+++ nexus/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'nexus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config(
