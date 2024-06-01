c6a0bd4c8563b728756729e99d5b308f2ef65882
JanetKabiru <126761051+JanetKabiru@users.noreply.github.com>
2024-06-01 13:46:45 +0300

    Update urls.py

--- nexus/urls.py
+++ nexus/urls.py
@@ -27,9 +27,8 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, 
-                            document_root=settings.STATIC_ROOT)                            
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)                            
 
     ##### Comment out for AWS media uploading ####
     # urlpatterns += static(settings.MEDIA_URL, 
