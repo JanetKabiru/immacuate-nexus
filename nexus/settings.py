95fb2b556799b3fa3dbe5265341d4032a9cefb89
JanetKabiru <126761051+JanetKabiru@users.noreply.github.com>
2024-06-01 12:54:22 +0300

    Update settings.py

--- nexus/settings.py
+++ nexus/settings.py
@@ -135,18 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # STORAGES = {
 #     "staticfiles": {
