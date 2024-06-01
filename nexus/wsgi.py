ed762083b05cff37676912b6c72b5b26940a152c
JanetKabiru <126761051+JanetKabiru@users.noreply.github.com>
2024-06-01 12:00:58 +0300

    Update wsgi.py

--- nexus/wsgi.py
+++ nexus/wsgi.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
-app = application
 
+app = application
