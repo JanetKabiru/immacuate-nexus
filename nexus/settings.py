78bd7067fe3c55e630479769e8b24e137fa5759f
JanetKabiru <126761051+JanetKabiru@users.noreply.github.com>
2024-06-01 11:18:51 +0300

    Update settings.py

--- nexus/settings.py
+++ nexus/settings.py
@@ -137,16 +137,19 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = (
+    BASE_DIR, 'static',
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # STORAGES = {
 #     "staticfiles": {
