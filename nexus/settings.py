6c2004133161d01911ad68e9bf686b63b7f5aee2
Wilfred Muema <64227473+Muia23@users.noreply.github.com>
2024-07-15 09:30:29 +0300

    Update allowed hosts settings

--- nexus/settings.py
+++ nexus/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", default="")
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','localhost','immaculatenexuscollege.or.ke']
 
 
 # Application definition
